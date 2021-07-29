42893313be392326abb316cc71124f387a5ac7e1
kawakawaDe <fdsam@hotmail.com>
2021-07-29 13:17:17 +0100

    debug

--- RPG/Source/HeroDebugModule/Private/HeroDebugModule.cpp
+++ RPG/Source/HeroDebugModule/Private/HeroDebugModule.cpp
@@ -1,5 +1,10 @@
 #include "HeroDebugModule/Public/HeroDebugModule.h"
 
+#if WITH_GAMEPLAY_DEBUGGER
+#include "HeroDebuggerCategory_Attributes.h"
+#include "GameplayDebugger.h"
+#endif
+
 DEFINE_LOG_CATEGORY(HeroDebugModule);
 
 #define LOCTEXT_NAMESPACE "FHeroDebugModule"
@@ -7,11 +12,28 @@ DEFINE_LOG_CATEGORY(HeroDebugModule);
 void FHeroDebugModule::StartupModule()
 {
 	UE_LOG(HeroDebugModule, Warning, TEXT("FHeroDebugModule module has started!"));
+
+#if WITH_GAMEPLAY_DEBUGGER
+	IGameplayDebugger& GameplayDebuggerModule = IGameplayDebugger::Get();
+
+	GameplayDebuggerModule.RegisterCategory("Character Attributtes", IGameplayDebugger::FOnGetCategory::CreateStatic(
+		&HeroDebuggerCategory_Attributes::MakeInstance), EGameplayDebuggerCategoryState::EnabledInSimulate);
+
+	GameplayDebuggerModule.NotifyCategoriesChanged();
+#endif
 }
 
 void FHeroDebugModule::ShutdownModule()
 {
 	UE_LOG(HeroDebugModule, Warning, TEXT("FHeroDebugModule module has shut down"));
+
+#if WITH_GAMEPLAY_DEBUGGER
+	if (IGameplayDebugger::IsAvailable())
+	{
+		IGameplayDebugger& GameplayDebuggerModule = IGameplayDebugger::Get();
+		GameplayDebuggerModule.UnregisterCategory("Character Attributtes");
+	}
+#endif
 }
 
 #undef LOCTEXT_NAMESPACE
